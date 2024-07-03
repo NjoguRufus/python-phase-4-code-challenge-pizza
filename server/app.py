67b0d05a6636de7ace2df6bbf81d4a00d3362047
NjoguRufus <rsgxliv@gmail.com>
2024-07-03 11:15:12 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request
 from flask_restful import Api, Resource
+from flask import Flask, request
 import os
+
+app = Flask(__name__)
+
+# Configure Flask app
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
@@ -44,7 +47,6 @@ class RestaurantDetails(Resource):
                 session.commit()
                 return {}, 204
             return {"error": "Restaurant not found"}, 404
-
 
 class PizzaList(Resource):
     def get(self):
